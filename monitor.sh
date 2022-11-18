6a38d4ac7601bf95eb1e3849c15c995aee42887f
Van Phan Dinh <vanphandinh@outlook.com>
2022-11-18 08:30:48 +0700

    testnet

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export TZ=Asia/Ho_Chi_Minh #TIMEZONE
+export TZ=Asia/Ho_Chi_Minh                                       #TIMEZONE
 TELEGRAM_BOT_TOKEN="55555555555:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" #YOUR TELEGRAM BOT TOKEN, CREATED VIA @BotFather
-CHAT_ID="YYYYYYYY" #DESTINATION TO RECEIVE NOTIFICATIONS
+CHAT_ID="YYYYYYYY"                                               #DESTINATION TO RECEIVE NOTIFICATIONS
 
 TOTAL_TXS=$(curl --location --max-time 180 -s -X POST 'http://127.0.0.1:9000' -H 'Content-Type: application/json' --data-raw '{ "jsonrpc":"2.0", "id":1, "method":"sui_getTotalTransactionNumber", "params":[] }' | jq '.result')
-API_STUCK=0
 TX_STUCK=0
+API_STUCK=0
 NOW=$(date +'%d/%m/%Y %T')
 CONTAINER_STARTED_AT=$(docker inspect -f '{{ .State.StartedAt }}' sui)
 CONTAINER_TIMESTAMP=$(date -d "$CONTAINER_STARTED_AT" +%s)
@@ -13,12 +13,11 @@ echo 'Sui Monitor Script v0.1'
 echo "$NOW - Current Txs: $TOTAL_TXS"
 
 notify() {
-  msg=$1 
-  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&parse_mode=html&text=$msg" &> /dev/null
+  msg=$1
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&parse_mode=html&text=$msg" &>/dev/null
 }
 
-while :
-do
+while :; do
   sleep 60s
   LATEST_TOTAL_TXS=$(curl --location --max-time 180 -s -X POST 'http://127.0.0.1:9000' -H 'Content-Type: application/json' --data-raw '{ "jsonrpc":"2.0", "id":1, "method":"sui_getTotalTransactionNumber", "params":[] }' | jq '.result')
   CONTAINER_STARTED_AT=$(docker inspect -f '{{ .State.StartedAt }}' sui)
@@ -27,14 +26,14 @@ do
 
   if [ "$LATEST_TOTAL_TXS" == "null" ]; then
     if [ "$API_STUCK" -gt "2" ]; then
-       API_STUCK=0
-       notify "<code>$NOW</code>"$'\n'"The API is stuck. Restarting node.."
-       echo "$NOW - The API is stuck. Restarting node.."
-       docker restart sui
+      API_STUCK=0
+      notify "<code>$NOW</code>"$'\n'"The API is stuck. Restarting node.."
+      echo "$NOW - The API is stuck. Restarting node.."
+      docker restart sui
     else
-       ((API_STUCK++))
-       notify "<code>$NOW</code>"$'\n'"The API got nothing"
-       echo "$NOW - The API got nothing"
+      ((API_STUCK++))
+      notify "<code>$NOW</code>"$'\n'"The API got nothing"
+      echo "$NOW - The API got nothing"
     fi
     continue
   fi
@@ -50,7 +49,7 @@ do
     notify "<code>$NOW</code>"$'\n'"Current Txs: $TOTAL_TXS"
     echo "$NOW - Current Txs: $TOTAL_TXS"
   else
-    if [ "$TX_STUCK" -gt "5" ]; then
+    if [ "$TX_STUCK" -gt "15" ]; then
       TX_STUCK=0
       notify "<code>$NOW</code>"$'\n'"The node is stuck. Restarting node.."
       echo "$NOW - The node is stuck. Restarting node.."
--- updater.sh
+++ updater.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-export TZ=Asia/Ho_Chi_Minh #TIMEZONE
+export TZ=Asia/Ho_Chi_Minh                                       #TIMEZONE
 TELEGRAM_BOT_TOKEN="55555555555:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" #YOUR TELEGRAM BOT TOKEN, CREATED VIA @BotFather
-CHAT_ID="YYYYYYYY" #DESTINATION TO RECEIVE NOTIFICATIONS
+CHAT_ID="YYYYYYYY"                                               #DESTINATION TO RECEIVE NOTIFICATIONS
 
 notify() {
-  msg=$1 
-  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&parse_mode=html&text=$msg" &> /dev/null
+  msg=$1
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&parse_mode=html&text=$msg" &>/dev/null
 }
 mkdir -p updates
+touch genesis.blob
+touch fullnode-template.yaml
 
 echo 'Sui Updater v0.1'
 
-while :
-do
+while :; do
   NOW=$(date +'%d/%m/%Y %T')
-  
+
   echo "$NOW - Checking genesis.blob.."
-  wget -q https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob -O ./updates/genesis.blob 
+  wget -q https://github.com/SuiExternal/sui-external/raw/main/genesis.blob -O ./updates/genesis.blob
   RETURN_CODE=$?
   if [ $RETURN_CODE -ne 0 ]; then
     echo "Download genesis.blob failed"
   else
-    md5f1=$(md5sum "./updates/genesis.blob" | cut -d' ' -f1) 
+    md5f1=$(md5sum "./updates/genesis.blob" | cut -d' ' -f1)
     md5f2=$(md5sum "genesis.blob" | cut -d' ' -f1)
     if [ "$md5f2" != "$md5f1" ]; then
-     cp ./updates/genesis.blob genesis.blob
-     docker compose down --volumes
-     docker compose up -d
-     notify "<code>$NOW</code>"$'\n'"New genesis.blob updated"
-     echo "New genesis.blob updated"
+      cp ./updates/genesis.blob genesis.blob
+      docker compose down --volumes
+      docker compose up -d
+      notify "<code>$NOW</code>"$'\n'"New genesis.blob updated"
+      echo "New genesis.blob updated"
     else
-     echo "No change"
+      echo "No change"
     fi
   fi
 
@@ -41,17 +42,16 @@ do
     echo "Download fullnode-template.yaml failed"
   else
     md5f2=$(md5sum "fullnode-template.yaml" | cut -d' ' -f1)
-    md5f1=$(md5sum "./updates/fullnode-template.yaml" | cut -d' ' -f1) 
+    md5f1=$(md5sum "./updates/fullnode-template.yaml" | cut -d' ' -f1)
     if [ "$md5f2" != "$md5f1" ]; then
-     cp ./updates/fullnode-template.yaml fullnode-template.yaml
-     docker restart sui
-     notify "<code>$NOW</code>"$'\n'"New fullnode-template.yaml updated"
-     echo "New fullnode-template.yaml updated"
+      cp ./updates/fullnode-template.yaml fullnode-template.yaml
+      docker restart sui
+      notify "<code>$NOW</code>"$'\n'"New fullnode-template.yaml updated"
+      echo "New fullnode-template.yaml updated"
     else
-     echo "No change"
+      echo "No change"
     fi
   fi
 
   sleep 60s
 done
-
